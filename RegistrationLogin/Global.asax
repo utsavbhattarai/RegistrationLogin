ec914840fafdba4b6ba1b10f3739dacb6f9f1caf
Utsav Bhattarai <utsav.bhattaraicode@gmail.com>
2018-01-16 21:01:20 -0600

    Add project files.

---
+++ RegistrationLogin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RegistrationLogin.MvcApplication" Language="C#" %>
---
+++ RegistrationLogin/Models/UserLogin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationLogin.Models
+{
+    public class UserLogin
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email Id Required")]
+        [Display(Name = "Email ID")]
+        public string EmailID { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password Required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
